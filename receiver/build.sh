4ace580b8feb6b5f9094be100db9aeb0decd024d
RavenbornJB <yarema.mishchenko@gmail.com>
2022-06-11 00:57:50 +0300

    another one

--- receiver/build.sh
+++ receiver/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t receive_tweets .
+docker build -f receiver/Dockerfile -t receive_tweets .
 docker run --name tweet-receiver --network kafka-network --rm receive_tweets
--- sender/build.sh
+++ sender/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t send_tweets .
+docker build -f sender/Dockerfile -t send_tweets .
 docker run --name tweet-sender --network kafka-network --rm send_tweets
